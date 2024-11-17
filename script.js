53a811284c05926fd7088d0cd77cf6706a9946ce
Rupali668 <165276802+Rupali668@users.noreply.github.com>
2024-11-17 21:06:25 +0530

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Smooth scrolling for anchor links
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
       e.preventDefault();
@@ -8,14 +8,14 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 
-// Get the "Listen to Our Music" link and the Our Album section
+
 const listenMusicLink = document.getElementById('listen-music');
 const albumSection = document.getElementById('album');
 
-// Add a click event listener to the "Listen to Our Music" link
-listenMusicLink.addEventListener('click', function (e) {
-  e.preventDefault(); // Prevent default link behavior
 
-  // Apply the zoom-out effect to the Our Album section
+listenMusicLink.addEventListener('click', function (e) {
+  e.preventDefault(); 
+
+  //zoom out
   albumSection.classList.toggle('zoomed-out');
 });
